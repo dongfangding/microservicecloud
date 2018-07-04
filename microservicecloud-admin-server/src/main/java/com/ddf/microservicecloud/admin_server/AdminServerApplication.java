9fe8301852d75421eacd63df7c9c72ef87301793
dongfang.ding <1041765757@qq.com>
2018-07-04 23:09:28 +0800

    新建boot-admin-server监控

---
+++ microservicecloud-admin-server/src/main/java/com/ddf/microservicecloud/admin_server/AdminServerApplication.java
@@ -0,0 +1,16 @@
+package com.ddf.microservicecloud.admin_server;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author DDf on 2018/7/4
+ */
+@SpringBootApplication
+@EnableAdminServer
+public class AdminServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+}
