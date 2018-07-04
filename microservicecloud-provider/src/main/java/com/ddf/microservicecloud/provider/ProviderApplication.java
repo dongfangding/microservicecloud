5c68e225b5d2ab8f6417ce3edc5d587ae26c3622
dongfang.ding <1041765757@qq.com>
2018-07-04 22:14:56 +0800

    UPDATE

--- microservicecloud-provider/src/main/java/com/ddf/microservicecloud/provider/ProviderApplication.java
+++ microservicecloud-provider/src/main/java/com/ddf/microservicecloud/provider/ProviderApplication.java
@@ -2,10 +2,15 @@ package com.ddf.microservicecloud.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -25,5 +30,15 @@ public class ProviderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProviderApplication.class, args);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(name = "redisTemplate")
+    public RedisTemplate<Object, Object> redisTemplate(
+            RedisConnectionFactory redisConnectionFactory){
+        RedisTemplate<Object, Object> template = new RedisTemplate<Object, Object>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setDefaultSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
     }
 }
